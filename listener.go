b00c6eb6641e20ed32b912da26654653f68fa76b
Marianne Feng <feng.marianne@gmail.com>
2017-11-19 20:10:08 -0800

    mushed code

--- listener.go
+++ listener.go
@@ -6,41 +6,10 @@ import (
 )
 //
 
-func sendResponse(conn *net.UDPConn, addr *net.UDPAddr) {
-	_,err := conn.WriteToUDP([]byte("From server: Hello I got your mesage "), addr)
-	if err != nil {
-		fmt.Printf("Couldn't send response %v", err)
-	}
-}
+
 
 func main() {
-    addr := net.UDPAddr{
-        Port: 1119,
-        IP:   net.ParseIP("0.0.0.0"),
-    }
-    conn, err := net.ListenUDP("udp", &addr)
-    // code does not block here
-    defer conn.Close()
-    if err != nil {
-        panic(err)
-    }
 
-	buf := make([]byte, 1024)
-    for {
-        rlen, remote, err := conn.ReadFromUDP(buf)
-        // Do stuff with the read bytes
-        fmt.Println(rlen)
-        fmt.Println(remote)
-        fmt.Println(string(buf[:rlen]))
-        if err != nil {
-            panic(err)
-        }
-        go sendResponse(conn, remote)
-    }
-
-    var testPayload = []byte("This is a test")
-
-    conn.Write(testPayload)
 
 }
 //
--- whereuat.go
+++ whereuat.go
@@ -70,6 +70,8 @@ func receivePong(pongNum int, pongChan <-chan Pong, doneChan chan<- []Pong) {
 }
 
 func main() {
+	go letsListen()
+
 	var host = ""
 	addrs, err := net.Interfaces()
 	if err != nil {
@@ -135,6 +137,36 @@ func checkUDP(addr string) {
 
 }
 
+func letsListen() {
+	addr := net.UDPAddr{
+		Port: 1119,
+		IP:   net.ParseIP("0.0.0.0"),
+	}
+	conn, err := net.ListenUDP("udp", &addr)
+	// code does not block here
+	defer conn.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	buf := make([]byte, 1024)
+	for {
+		rlen, remote, err := conn.ReadFromUDP(buf)
+		// Do stuff with the read bytes
+		fmt.Println(rlen)
+		fmt.Println(remote)
+		fmt.Println(string(buf[:rlen]))
+		if err != nil {
+			panic(err)
+		}
+		go sendResponse(conn, remote)
+	}
+
+	var testPayload = []byte("This is a test")
+
+	conn.Write(testPayload)
+}
+
 func letsTalk(conn net.Conn, p []byte) {
 	fmt.Fprintf(conn, "haygurl")
 
@@ -147,5 +179,14 @@ func letsTalk(conn net.Conn, p []byte) {
 			break
 		}
 	}
+
 	conn.Close()
+}
+
+
+func sendResponse(conn *net.UDPConn, addr *net.UDPAddr) {
+	_,err := conn.WriteToUDP([]byte("From server: Hello I got your mesage "), addr)
+	if err != nil {
+		fmt.Printf("Couldn't send response %v", err)
+	}
 }
