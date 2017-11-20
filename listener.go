ebe83308439997ff4f712ce95baca1be3392b66d
Jessi Shank <jessishank@level11.com>
2017-11-19 20:06:23 -0800

    listener

--- listener.go
+++ listener.go
@@ -4,6 +4,14 @@ import (
     "fmt"
     "net"
 )
+//
+
+func sendResponse(conn *net.UDPConn, addr *net.UDPAddr) {
+	_,err := conn.WriteToUDP([]byte("From server: Hello I got your mesage "), addr)
+	if err != nil {
+		fmt.Printf("Couldn't send response %v", err)
+	}
+}
 
 func main() {
     addr := net.UDPAddr{
@@ -17,19 +25,51 @@ func main() {
         panic(err)
     }
 
-    var buf [1024]byte
+	buf := make([]byte, 1024)
     for {
-        rlen, remote, err := conn.ReadFromUDP(buf[:])
+        rlen, remote, err := conn.ReadFromUDP(buf)
         // Do stuff with the read bytes
         fmt.Println(rlen)
         fmt.Println(remote)
+        fmt.Println(string(buf[:rlen]))
         if err != nil {
             panic(err)
         }
+        go sendResponse(conn, remote)
     }
 
-    var testPayload []byte = []byte("This is a test")
+    var testPayload = []byte("This is a test")
 
     conn.Write(testPayload)
 
 }
+//
+//func sendResponse(conn *net.UDPConn, addr *net.UDPAddr) {
+//	_,err := conn.WriteToUDP([]byte("From server: Hello I got your mesage "), addr)
+//	if err != nil {
+//		fmt.Printf("Couldn't send response %v", err)
+//	}
+//}
+//
+//
+//func main() {
+//	p := make([]byte, 2048)
+//	addr := net.UDPAddr{
+//		Port: 1119,
+//		IP: net.ParseIP("0.0.0.0"),
+//	}
+//	ser, err := net.ListenUDP("udp", &addr)
+//	if err != nil {
+//		fmt.Printf("Some error %v\n", err)
+//		return
+//	}
+//	for {
+//		_,remoteaddr,err := ser.ReadFromUDP(p)
+//		fmt.Printf("Read a message from %v %s \n", remoteaddr, p)
+//		if err !=  nil {
+//			fmt.Printf("Some error  %v", err)
+//			continue
+//		}
+//		go sendResponse(ser, remoteaddr)
+//	}
+//}
